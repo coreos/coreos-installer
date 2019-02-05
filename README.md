d547671848e54a1c8f78306233e05a4fc5343350
Dusty Mabe <dusty@dustymabe.com>
2019-02-05 02:42:01 -0500

    dracut: kernel cmdline: use coreos.inst as prefix
    We'll have coreos.inst denote the options we'll use. Similar
    to how anaconda uses inst.xxxx.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ via a dracut module.
 ## MORE DOCS TO COME
 
 ## Kernel command line options for coreos-installer
-* coreos.install_dev - The block device on the system to install to
-* coreos.image_url - The url of the coreos image to install to this device
-* coreos.ignition_url - The url of the coreos ignition config (optional, enter
-  coreos.ignition_url=skip to not load an ignition config)
+* coreos.inst.install_dev - The block device on the system to install to
+* coreos.inst.image_url - The url of the coreos image to install to this device
+* coreos.inst.ignition_url - The url of the coreos ignition config (optional, enter
+  coreos.inst.ignition_url=skip to not load an ignition config)
--- dracut-module/parse-coreos.sh
+++ dracut-module/parse-coreos.sh
@@ -3,34 +3,34 @@
 . /lib/dracut-lib.sh
 
 
-local IMAGE_URL=$(getarg coreos.image_url=)
+local IMAGE_URL=$(getarg coreos.inst.image_url=)
 if [ ! -z "$IMAGE_URL" ]
 then
 	echo "preset image_url to $IMAGE_URL" >> /tmp/debug
 	echo $IMAGE_URL >> /tmp/image_url
 fi
 
-local DEST_DEV=$(getarg coreos.install_dev=)
+local DEST_DEV=$(getarg coreos.inst.install_dev=)
 if [ ! -z "$DEST_DEV" ]
 then
 	echo "preset install dev to $DEST_DEV" >> /tmp/debug
 	echo $DEST_DEV >> /tmp/selected_dev
 fi
 
-local IGNITION_URL=$(getarg coreos.ignition_url=)
+local IGNITION_URL=$(getarg coreos.inst.ignition_url=)
 if [ ! -z "$IGNITION_URL" ]
 then
 	echo "preset ignition url to $IGNITION_URL" >> /tmp/debug
 	echo $IGNITION_URL >> /tmp/ignition_url
 fi
 
-if getargbool 0 coreos.skip_media_check
+if getargbool 0 coreos.inst.skip_media_check
 then
 	echo "Asserting skip of media check" >> /tmp/debug
 	echo 1 > /tmp/skip_media_check
 fi
 
-if getargbool 0 coreos.skip_reboot 
+if getargbool 0 coreos.inst.skip_reboot
 then
 	echo "Asserting reboot skip" >> /tmp/debug
 	echo 1 > /tmp/skip_reboot
