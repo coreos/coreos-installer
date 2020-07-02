coreos/coreos-installer

commit 0fc90509298d7ccf49c22ccaa825ad2c6db126d5

install: Fail early if image sector size doesn't match destination
Otherwise we'd copy the whole image, then complain that we couldn't
find any partitions.  Detect the image sector size by checking both
possible offsets for a GPT, detect the destination sector size by ioctl,
and fail if they don't match.

If we don't detect any GPT, continue without error.  In the future,
detection could be expanded to cover additional partition table types.

src/download.rs
@@ -19,11 +19,13 @@ use nix::unistd::isatty;
 use progress_streams::ProgressReader;
 use std::fs::{remove_file, File, OpenOptions};
 use std::io::{copy, stderr, BufRead, BufReader, Read, Seek, SeekFrom, Write};
+use std::num::NonZeroU32;
 use std::os::unix::io::AsRawFd;
 use std::path::{Path, PathBuf};
 use std::time::{Duration, Instant};
 use xz2::read::XzDecoder;
 
+use crate::blockdev::detect_formatted_sector_size;
 use crate::cmdline::*;
 use crate::errors::*;
 use crate::source::*;
@@ -141,7 +143,8 @@ fn write_image_and_sig(
         .chain_err(|| format!("opening {}", path.display()))?;
 
     // download and verify image
-    write_image(source, &mut dest, decompress)?;
+    // don't check sector size
+    write_image(source, &mut dest, decompress, None)?;
 
     // write signature, if relevant
     if !decompress && source.signature.is_some() {
@@ -160,7 +163,12 @@ fn write_image_and_sig(
 }
 
 /// Copy the image to disk and verify its signature.
-pub fn write_image(source: &mut ImageSource, dest: &mut File, decompress: bool) -> Result<()> {
+pub fn write_image(
+    source: &mut ImageSource,
+    dest: &mut File,
+    decompress: bool,
+    expected_sector_size: Option<NonZeroU32>,
+) -> Result<()> {
     // wrap source for GPG verification
     let mut verify_reader: Box<dyn Read> = {
         if let Some(signature) = source.signature.as_ref() {
@@ -239,15 +247,32 @@ pub fn write_image(source: &mut ImageSource, dest: &mut File, decompress: bool) 
         }
     };
 
-    // Cache the first MiB of input and write zeroes instead.  This ensures
-    // that the disk image can't be used accidentally before its GPG signature
-    // is verified.
+    // Read the first MiB of input and, if requested, check it against the
+    // image's formatted sector size.
     let mut first_mb: [u8; 1024 * 1024] = [0; 1024 * 1024];
-    dest.write_all(&first_mb)
-        .chain_err(|| "clearing first MiB of disk")?;
     decompress_reader
         .read_exact(&mut first_mb)
         .chain_err(|| "decoding first MiB of image")?;
+    // Were we asked to check sector size?
+    if let Some(expected) = expected_sector_size {
+        // Can we derive one from source data?
+        if let Some(actual) = detect_formatted_sector_size(&first_mb) {
+            // Do they match?
+            if expected != actual {
+                bail!(
+                    "source has sector size {} but destination has sector size {}",
+                    actual.get(),
+                    expected.get()
+                );
+            }
+        }
+    }
+
+    // Cache the first MiB and write zeroes to dest instead.  This ensures
+    // that the disk image can't be used accidentally before its GPG signature
+    // is verified.
+    dest.write_all(&first_mb)
+        .chain_err(|| "clearing first MiB of disk")?;
 
     // do the rest of the copy
     // This physically writes any runs of zeroes, rather than sparsifying,

src/blockdev.rs
@@ -14,11 +14,14 @@
 
 use error_chain::bail;
 use nix::errno::Errno;
-use nix::{self, ioctl_none, ioctl_write_ptr_bad, mount, request_code_none};
+use nix::{self, ioctl_none, ioctl_read_bad, ioctl_write_ptr_bad, mount, request_code_none};
 use regex::Regex;
 use std::collections::HashMap;
+use std::convert::TryInto;
 use std::fs::{remove_dir, File};
 use std::io::{Seek, SeekFrom};
+use std::num::NonZeroU32;
+use std::os::raw::c_int;
 use std::os::unix::io::AsRawFd;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -204,6 +207,21 @@ pub fn reread_partition_table(file: &mut File) -> Result<()> {
     Ok(())
 }
 
+/// Get the logical sector size of a block device.
+pub fn get_sector_size(file: &mut File) -> Result<NonZeroU32> {
+    let fd = file.as_raw_fd();
+    let mut size: c_int = 0;
+    match unsafe { blksszget(fd, &mut size) } {
+        Ok(_) => {
+            let size_u32: u32 = size
+                .try_into()
+                .chain_err(|| format!("sector size {} doesn't fit in u32", size))?;
+            NonZeroU32::new(size_u32).ok_or_else(|| "found sector size of zero".into())
+        }
+        Err(e) => Err(Error::with_chain(e, "getting sector size")),
+    }
+}
+
 /// Try discarding all blocks from the underlying block device.
 /// Return true if successful, false if the underlying device doesn't
 /// support discard, or an error otherwise.
@@ -232,6 +250,7 @@ pub fn try_discard_all(file: &mut File) -> Result<bool> {
 
 // create unsafe ioctl wrappers
 ioctl_none!(blkrrpart, 0x12, 95);
+ioctl_read_bad!(blksszget, request_code_none!(0x12, 104), c_int);
 ioctl_write_ptr_bad!(blkdiscard, request_code_none!(0x12, 119), [u64; 2]);
 
 pub fn udev_settle() -> Result<()> {
@@ -257,6 +276,23 @@ pub fn udev_settle() -> Result<()> {
         bail!("udevadm settle failed");
     }
     Ok(())
+}
+
+/// Inspect a buffer from the start of a disk image and return its formatted
+/// sector size, if any can be determined.
+pub fn detect_formatted_sector_size(buf: &[u8]) -> Option<NonZeroU32> {
+    let gpt_magic: &[u8; 8] = b"EFI PART";
+
+    if buf.len() >= 520 && buf[512..520] == gpt_magic[..] {
+        // GPT at offset 512
+        NonZeroU32::new(512)
+    } else if buf.len() >= 4104 && buf[4096..4104] == gpt_magic[..] {
+        // GPT at offset 4096
+        NonZeroU32::new(4096)
+    } else {
+        // Unknown
+        None
+    }
 }
 
 #[cfg(test)]

src/install.rs
@@ -101,8 +101,11 @@ fn write_disk(
     // leveler or LVM thin pool.
     try_discard_all(dest)?;
 
+    // Get sector size of destination, for comparing with image
+    let sector_size = get_sector_size(dest)?;
+
     // copy the image
-    write_image(source, dest, true)?;
+    write_image(source, dest, true, Some(sector_size))?;
     reread_partition_table(dest)?;
     udev_settle()?;
 

commit 317f1657624d98e03789f3c5e6ecddb95fe129f5

download: correctly zero first MiB of disk during initial write
We were accidentally writing out the real data, and then going back
afterward and writing it again.

Fixes: 0fc90509298d ("install: Fail early if image sector size doesn't match destination")

src/download.rs
@@ -271,7 +271,7 @@ pub fn write_image(
     // Cache the first MiB and write zeroes to dest instead.  This ensures
     // that the disk image can't be used accidentally before its GPG signature
     // is verified.
-    dest.write_all(&first_mb)
+    dest.write_all(&[0u8; 1024 * 1024])
         .chain_err(|| "clearing first MiB of disk")?;
 
     // do the rest of the copy