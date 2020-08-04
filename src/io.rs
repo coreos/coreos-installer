20f277601f0df9600c72c85e8ba2609814c55731
Benjamin Gilbert <bgilbert@redhat.com>
2020-08-04 18:24:26 -0400

    io: add a format-sniffing decompressing Reader

--- src/io.rs
+++ src/io.rs
@@ -13,9 +13,11 @@
 // limitations under the License.
 
 use error_chain::{bail, ensure};
+use flate2::read::GzDecoder;
 use openssl::sha;
-use std::io::{self, ErrorKind, Read, Write};
+use std::io::{self, BufRead, ErrorKind, Read, Write};
 use std::result;
+use xz2::read::XzDecoder;
 
 use crate::errors::*;
 
@@ -164,6 +166,43 @@ impl IgnitionHash {
     }
 }
 
+enum CompressDecoder<R: BufRead> {
+    Uncompressed(R),
+    Gzip(GzDecoder<R>),
+    Xz(XzDecoder<R>),
+}
+
+pub struct DecompressReader<R: BufRead> {
+    decoder: CompressDecoder<R>,
+}
+
+/// Format-sniffing decompressor
+impl<R: BufRead> DecompressReader<R> {
+    pub fn new(mut source: R) -> Result<Self> {
+        use CompressDecoder::*;
+        let sniff = source.fill_buf().chain_err(|| "sniffing input")?;
+        let decoder = if sniff.len() > 2 && &sniff[0..2] == b"\x1f\x8b" {
+            Gzip(GzDecoder::new(source))
+        } else if sniff.len() > 6 && &sniff[0..6] == b"\xfd7zXZ\x00" {
+            Xz(XzDecoder::new(source))
+        } else {
+            Uncompressed(source)
+        };
+        Ok(Self { decoder })
+    }
+}
+
+impl<R: BufRead> Read for DecompressReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> result::Result<usize, io::Error> {
+        use CompressDecoder::*;
+        match &mut self.decoder {
+            Uncompressed(d) => d.read(buf),
+            Gzip(d) => d.read(buf),
+            Xz(d) => d.read(buf),
+        }
+    }
+}
+
 pub struct LimitReader<R: Read> {
     source: R,
     length: u64,
@@ -173,7 +212,7 @@ pub struct LimitReader<R: Read> {
 
 impl<R: Read> LimitReader<R> {
     pub fn new(source: R, length: u64, limit_cause: String) -> Self {
-        LimitReader {
+        Self {
             source,
             length,
             remaining: length,
