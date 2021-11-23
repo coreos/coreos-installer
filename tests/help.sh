78fad45f4ce588064b333fd2cab5d7c9a1c2b9f2
Benjamin Gilbert <bgilbert@redhat.com>
2021-11-23 01:00:18 -0500

    tests: check docs for undocumented subcommands and options
    Ignore hidden subcommands.

--- tests/help.sh
+++ tests/help.sh
@@ -8,17 +8,44 @@ help() {
     "${rootdir}/target/debug/coreos-installer" $* --help
 }
 
+hidden=
 fail=0
 total=0
 checklen() {
-    local length subcommand subcommands
+    local length subcommand subcommands docfile docfile_display doc_section
+    local longopts shortopts opt
     total=$((${total} + 1))
     echo "Checking coreos-installer $*..."
+
     length=$(help $* | wc -L)
     if [ "${length}" -gt 80 ] ; then
         echo "$* --help line length ${length} > 80"
         fail=1
     fi
+
+    longopts=$(help $* | (grep -Eo -- "--[a-zA-Z0-9_-]+" ||:))
+    shortopts=$(help $* | (grep -Eo -- "-[a-zA-Z0-9]," ||:) | tr -d ,)
+    # Ignore subcommands with subcommands, and hidden subcommands
+    if [ -z "${hidden}" ] && ! help $* | grep -q "SUBCOMMANDS:" ; then
+        docfile="${rootdir}/docs/cmd/$1.md"
+        docfile_display="$(realpath --relative-to=${rootdir} ${docfile})"
+        doc_section=$(awk "/^# coreos-installer / {active=0} /^# coreos-installer $*$/ {active=1} {if (active) {print}}" "${docfile}")
+        if [ -n "${doc_section}" ]; then
+            for opt in ${longopts} ${shortopts}; do
+                if [[ ${opt} == @(-h|--help) ]]; then
+                    continue
+                fi
+                if ! echo "${doc_section}" | grep -qF -- "**${opt}**"; then
+                    echo "$* ${opt} not documented in ${docfile_display}"
+                    fail=1
+                fi
+            done
+        else
+            echo "$* not documented in ${docfile_display}"
+            fail=1
+        fi
+    fi
+
     subcommands=$(help $* | awk 'BEGIN {subcommands=0} {if (subcommands) print $1} /SUBCOMMANDS:/ {subcommands=1}')
     for subcommand in ${subcommands}; do
         checklen $* ${subcommand}
@@ -32,6 +59,7 @@ if [ ${total} -lt 2 ]; then
 fi
 
 # Hidden commands that users might invoke anyway (i.e. deprecated ones)
+hidden=1
 checklen iso embed
 checklen iso show
 checklen iso remove
