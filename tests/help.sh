edb19a58af49f2370397dbac2ac3f21135d22826
Benjamin Gilbert <bgilbert@redhat.com>
2021-11-24 13:59:12 -0500

    tests/help: allow running from outside the repo root

--- tests/help.sh
+++ tests/help.sh
@@ -3,18 +3,23 @@
 
 set -euo pipefail
 
+rootdir="$(dirname $0)/.."
+help() {
+    "${rootdir}/target/debug/coreos-installer" $* --help
+}
+
 fail=0
 total=0
 checklen() {
     local length subcommand subcommands
     total=$((${total} + 1))
     echo "Checking coreos-installer $*..."
-    length=$(target/debug/coreos-installer $* --help | wc -L)
+    length=$(help $* | wc -L)
     if [ "${length}" -gt 80 ] ; then
         echo "$* --help line length ${length} > 80"
         fail=1
     fi
-    subcommands=$(target/debug/coreos-installer $* --help | awk 'BEGIN {subcommands=0} {if (subcommands) print $1} /SUBCOMMANDS:/ {subcommands=1}')
+    subcommands=$(help $* | awk 'BEGIN {subcommands=0} {if (subcommands) print $1} /SUBCOMMANDS:/ {subcommands=1}')
     for subcommand in ${subcommands}; do
         checklen $* ${subcommand}
     done
