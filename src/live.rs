coreos/coreos-installer

commit 7b68ee0c8949eef91329fa49b2edaa16ca65e9bb

live: find ISO Ignition embed area using ISO9660 parser
Use /images/ignition.img as the Ignition embed area.  Drop support for
the coreiso+ header, since the header points to that file in all supported
OS releases (FCOS and RHCOS 4.6+).

src/live.rs
@@ -31,7 +31,7 @@ use crate::io::*;
 use crate::iso9660::{self, IsoFs};
 
 const FILENAME: &str = "config.ign";
-const COREOS_IGNITION_HEADER_MAGIC: &[u8] = b"coreiso+";
+const COREOS_IGNITION_EMBED_PATH: &str = "IMAGES/IGNITION.IMG";
 const COREOS_IGNITION_HEADER_SIZE: u64 = 24;
 const COREOS_KARG_EMBED_AREA_HEADER_MAGIC: &[u8] = b"coreKarg";
 const COREOS_KARG_EMBED_AREA_HEADER_SIZE: u64 = 72;
@@ -265,8 +265,10 @@ struct IsoConfig {
 
 impl IsoConfig {
     pub fn for_file(file: &mut File) -> Result<Self> {
+        let mut iso = IsoFs::from_file(file.try_clone().context("cloning file")?)
+            .context("parsing ISO9660 image")?;
         Ok(Self {
-            ignition: ignition_embed_area(file)?,
+            ignition: ignition_embed_area(&mut iso)?,
             kargs: KargEmbedAreas::for_file(file)?,
         })
     }
@@ -579,31 +581,14 @@ impl KargEmbedAreas {
     }
 }
 
-fn ignition_embed_area(file: &mut File) -> Result<Region> {
-    // The ISO 9660 System Area is 32 KiB.  The last 24 bytes should be:
-    // 8 bytes: magic string "coreiso+"
-    // 8 bytes little-endian: offset of embed area from start of file
-    // 8 bytes little-endian: length of embed area
-    let region = Region::read(
-        file,
-        32768 - COREOS_IGNITION_HEADER_SIZE,
-        COREOS_IGNITION_HEADER_SIZE as usize,
-    )
-    .context("reading Ignition embed header")?;
-    let mut header = &region.contents[..];
-    // magic number
-    if header.copy_to_bytes(8) != COREOS_IGNITION_HEADER_MAGIC {
-        bail!("Unrecognized CoreOS ISO image.");
-    }
-    // offset
-    let offset = header.get_u64_le();
-    // length
-    let length: usize = header
-        .get_u64_le()
-        .try_into()
-        .context("embed area too large to allocate")?;
+fn ignition_embed_area(iso: &mut IsoFs) -> Result<Region> {
+    let f = iso
+        .get_path(COREOS_IGNITION_EMBED_PATH)
+        .context("finding Ignition embed area")?
+        .try_into_file()?;
     // read (checks offset/length as a side effect)
-    Region::read(file, offset, length).context("reading Ignition embed area")
+    Region::read(iso.as_file()?, f.address.as_offset(), f.length as usize)
+        .context("reading Ignition embed area")
 }
 
 /// Make a gzipped CPIO archive containing the specified Ignition config.

commit 5b9be4412db5b13464927be7e09fb445a14a4adb

Improve error messages when ISO features aren't supported
7b68ee0c8949 changed the error on a non-CoreOS ISO image from
"Unrecognized CoreOS ISO image." to "finding initrd embed area:
intermediate directory IMAGES does not exist".  Change it back, and also
improve the errors from iso extract pxe and iso extract minimal-iso.

Fixes: 7b68ee0c8949 ("live: find ISO Ignition embed area using ISO9660 parser")

src/live.rs
@@ -545,7 +545,7 @@ impl IsoConfig {
 
     pub fn for_iso(iso: &mut IsoFs) -> Result<Self> {
         Ok(Self {
-            initrd: InitrdEmbedArea::for_iso(iso)?,
+            initrd: InitrdEmbedArea::for_iso(iso).context("Unrecognized CoreOS ISO image.")?,
             kargs: KargEmbedAreas::for_iso(iso)?,
         })
     }
@@ -1367,7 +1367,10 @@ pub fn iso_inspect(config: IsoInspectConfig) -> Result<()> {
 
 pub fn iso_extract_pxe(config: IsoExtractPxeConfig) -> Result<()> {
     let mut iso = IsoFs::from_file(open_live_iso(&config.input, None)?)?;
-    let pxeboot = iso.get_path(COREOS_ISO_PXEBOOT_DIR)?.try_into_dir()?;
+    let pxeboot = iso
+        .get_path(COREOS_ISO_PXEBOOT_DIR)
+        .context("Unrecognized CoreOS ISO image.")?
+        .try_into_dir()?;
     create_dir_all(&config.output_dir)?;
 
     let base = {
@@ -1440,10 +1443,15 @@ pub fn iso_extract_minimal_iso(config: IsoExtractMinimalIsoConfig) -> Result<()>
         copy_file_from_iso(&mut full_iso, &rootfs, Path::new(path))?;
     }
 
-    let miniso_data_file = full_iso
-        .get_path(COREOS_ISO_MINISO_FILE)
-        .with_context(|| format!("looking up '{}'", COREOS_ISO_MINISO_FILE))?
-        .try_into_file()?;
+    let miniso_data_file = match full_iso.get_path(COREOS_ISO_MINISO_FILE) {
+        Ok(record) => record.try_into_file()?,
+        Err(e) if e.is::<iso9660::NotFound>() => {
+            bail!("This ISO image does not support extracting a minimal ISO.")
+        }
+        Err(e) => {
+            return Err(e).with_context(|| format!("looking up '{}'", COREOS_ISO_MINISO_FILE))
+        }
+    };
 
     let data = {
         let mut f = full_iso.read_file(&miniso_data_file)?;

tests/customize.sh
@@ -209,6 +209,10 @@ xz -dc "${rootdir}/fixtures/iso/embed-areas-2020-09.iso.xz" > old.iso
     grep -q "does not support customizing live kernel arguments"
 coreos-installer iso customize old.iso \
     --pre-install "${fixtures}/pre-install-1"
+xz -dc "${rootdir}/fixtures/iso/synthetic.iso.xz" > old.iso
+(coreos-installer iso customize old.iso \
+    --pre-install "${fixtures}/pre-install-1" 2>&1 ||:) |
+    grep -q "Unrecognized CoreOS ISO image"
 # no-op
 coreos-installer iso customize src-iso -o iso
 