026edd257f020383eb1756e35069af9ff08fa277
Nikita Dubrovskii <nikita@linux.ibm.com>
2022-04-26 14:29:04 +0200

    s390x: support secure execution
    This brings support for systems running with enabled IBM Secure Execution.
    Now rdcore/installer knows how to genereate 'sd-boot' image and 'zipl' it.
    If system uses LUKS encrypted boot and root filesystems, than crypttab and
    contents of /ets/luks/* are added to 'sd-boot' image.
    
    Signed-off-by: Nikita Dubrovskii <nikita@linux.ibm.com>

--- src/s390x/zipl.rs
+++ src/s390x/zipl.rs
@@ -12,15 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::io::{visit_bls_entry_options, KargsEditor};
+use crate::blockdev::Mount;
+use crate::io::{visit_bls_entry, visit_bls_entry_options, Initrd, KargsEditor};
 use crate::runcmd;
+use crate::util::cmd_output;
 use anyhow::{anyhow, Context, Result};
+use nix::mount::MsFlags;
 use regex::Regex;
-use std::fs::{copy, create_dir_all, read_dir};
-use std::io::Write;
-use std::path::Path;
+use std::fs::{copy, create_dir_all, read_dir, DirEntry, File};
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
 use std::process::Command;
-use tempfile::Builder;
+use tempfile::{Builder, NamedTempFile};
 
 /// Sets the boot device to `dev` using `chreipl`.
 pub fn chreipl<P: AsRef<Path>>(dev: P) -> Result<()> {
@@ -29,66 +32,212 @@ pub fn chreipl<P: AsRef<Path>>(dev: P) -> Result<()> {
     Ok(())
 }
 
+fn secure_execution_is_enabled() -> Result<bool> {
+    let sysfs_flag = "/sys/firmware/uv/prot_virt_guest";
+    match File::open(sysfs_flag) {
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(false),
+        Err(e) => Err(e).with_context(|| format!("reading {}", sysfs_flag)),
+        Ok(mut f) => {
+            let mut buffer = String::new();
+            f.read_to_string(&mut buffer)?;
+            Ok(buffer.trim() == "1")
+        }
+    }
+}
+
+fn find_files<P: AsRef<Path>>(
+    path: P,
+    f: impl Fn(&DirEntry) -> Result<bool>,
+) -> Result<Vec<PathBuf>> {
+    read_dir(&path)
+        .with_context(|| format!("reading directory {}", path.as_ref().display()))?
+        .filter_map(|r| {
+            r.map_err(anyhow::Error::new)
+                .and_then(|ent| f(&ent).map(|b| b.then(|| ent.path())))
+                .transpose()
+        })
+        .collect::<Result<Vec<_>>>()
+}
+
+fn regenerate_initrd<P: AsRef<Path>>(source: P, files: &[PathBuf]) -> Result<NamedTempFile> {
+    let source = source.as_ref();
+    let mut dest = Builder::new()
+        .prefix("initrd")
+        .suffix(".img")
+        .append(true)
+        .tempfile()
+        .context("creating cpio for extras")?;
+
+    // copying original initrd to tmpfile
+    let mut initrd = File::open(source)?;
+    std::io::copy(&mut initrd, &mut dest)
+        .with_context(|| format!("copying {} to {}", source.display(), dest.path().display()))?;
+
+    let mut initrd = Initrd::default();
+    for path in files {
+        let contents =
+            std::fs::read(path).with_context(|| format!("reading {}", path.display()))?;
+        let path = path
+            .to_str()
+            .with_context(|| format!("path {} is not UTF-8", path.display()))?;
+        initrd.add(path, contents);
+    }
+
+    // appending
+    let initrd = initrd.to_bytes()?;
+    dest.as_file_mut()
+        .write(&initrd)
+        .with_context(|| format!("appending luks-initrd to {}", dest.path().display()))?;
+    Ok(dest)
+}
+
+fn get_info_from_bls(boot: &Path) -> Result<(String, String, String)> {
+    let mut kernel = None;
+    let mut initrd = None;
+    let mut options = None;
+
+    let read_opts = |contents: &str| {
+        for l in contents.lines() {
+            match l.split_once(' ') {
+                Some(("linux", s)) => kernel = Some(s.trim().to_owned()),
+                Some(("initrd", s)) => initrd = Some(s.trim().to_owned()),
+                Some(("options", s)) => options = Some(s.trim().to_owned()),
+                _ => {}
+            }
+        }
+        Ok(None)
+    };
+    visit_bls_entry(boot, read_opts)?;
+
+    let kernel = kernel.ok_or_else(|| anyhow!("missing 'linux' key in default BLS config"))?;
+    let initrd = initrd.ok_or_else(|| anyhow!("missing 'initrd' key in default BLS config"))?;
+    let options = options.ok_or_else(|| anyhow!("missing 'options' key in default BLS config"))?;
+
+    Ok((kernel, initrd, options))
+}
+
+fn generate_sdboot(mountpoint: &Path, boot: &Path) -> Result<PathBuf> {
+    let (kernel, initrd, options) = get_info_from_bls(boot)?;
+
+    // we need a full path to kernel and initrd
+    let kernel = boot.join(&kernel[1..]);
+    let initrd = boot.join(&initrd[1..]);
+
+    let mut cmdline = Builder::new()
+        .prefix("se-cmdline.")
+        .tempfile()
+        .context("creating zipl se cmdline")?;
+    cmdline
+        .write_all(options.as_bytes())
+        .context("writing zipl se cmdline")?;
+
+    let hostkeys = find_files("/etc/se-hostkeys", |e: &DirEntry| {
+        Ok(e.file_name()
+            .to_str()
+            .map(|p| p.starts_with("ibm-z-hostkey-"))
+            .unwrap_or_default())
+    })?;
+
+    let initrd = {
+        let mut extra = find_files("/etc/luks/", |e: &DirEntry| Ok(e.metadata()?.is_file()))?;
+        extra.push(PathBuf::from("/etc/crypttab"));
+        regenerate_initrd(&initrd, &extra)?
+    };
+
+    let sdboot = mountpoint.join("sdboot");
+    let mut cmd = Command::new("genprotimg");
+    cmd.arg("-V")
+        .arg("-i")
+        .arg(kernel)
+        .arg("-r")
+        .arg(initrd.path())
+        .arg("-p")
+        .arg(cmdline.path())
+        .arg("--no-verify")
+        .arg("-o")
+        .arg(&sdboot);
+    for k in hostkeys {
+        cmd.arg("-k").arg(k);
+    }
+    cmd_output(&mut cmd)?;
+    Ok(sdboot)
+}
+
 /// Runs `zipl` based on Ignition and BLS configuration in `boot`.
 pub fn zipl<P: AsRef<Path>>(boot: P) -> Result<()> {
     let boot = boot.as_ref();
 
-    // create dummy config for zipl
-    let mut conffile = Builder::new()
-        .prefix("coreos-installer-zipl.")
-        .tempfile()
-        .context("creating zipl config")?;
-    let data = format!(
-        "[defaultboot]\ndefaultauto\nprompt=1\ntimeout=5\nsecure=auto\ntarget={}\n",
-        boot.to_str().unwrap()
-    );
-    conffile
-        .write_all(data.as_bytes())
-        .context("writing zipl config")?;
+    if secure_execution_is_enabled()? {
+        // Secure Execution is only supported with pre-built qemu-secex image
+        let target = Mount::try_mount("/dev/disk/by-label/se", "ext4", MsFlags::empty())?;
+        let sdboot = generate_sdboot(target.mountpoint(), boot)?;
 
-    // we have to copy bls config files for further modification
-    let tempdir = Builder::new()
-        .prefix("coreos-installer-zipl-bls-")
-        .tempdir()
-        .context("creating temporary directory")?;
-    let firstboot_file = boot.join("ignition.firstboot");
-    let blsdir = if firstboot_file.exists() {
-        let blsdir = tempdir.path().join("loader/entries");
-        create_dir_all(&blsdir).with_context(|| format!("creating {}", blsdir.display()))?;
-        read_dir(boot.join("loader/entries"))
-            .with_context(|| format!("reading {}", boot.display()))?
-            .into_iter()
-            .filter_map(Result::ok)
-            .filter(|p| p.file_type().unwrap().is_file())
-            .for_each(|src| {
-                copy(src.path(), blsdir.join(src.file_name())).unwrap();
-            });
-        let mut extra = vec!["ignition.firstboot".to_string()];
-        let firstboot_contents = std::fs::read_to_string(&firstboot_file)
-            .with_context(|| format!("reading \"{}\"", firstboot_file.display()))?;
-        if let Some(firstboot_kargs) = extract_firstboot_kargs(&firstboot_contents)? {
-            extra.extend_from_slice(
-                &firstboot_kargs
-                    .split_whitespace()
-                    .map(|s| s.to_string())
-                    .collect::<Vec<String>>(),
-            );
-        }
-        visit_bls_entry_options(tempdir.path(), |orig_options: &str| {
-            KargsEditor::new()
-                .append_if_missing(extra.as_slice())
-                .maybe_apply_to(orig_options)
-        })
-        .with_context(|| format!("appending {:?}", extra))?;
-
-        blsdir
+        runcmd!(
+            "zipl",
+            "-V",
+            "--target",
+            target.mountpoint(),
+            "--image",
+            sdboot
+        )
     } else {
-        boot.join("loader/entries")
-    };
+        // This branch could be also executed during installation, that's why
+        // we have to take care of ignition.firstboot karg and copy bls config
+        // files for further modification
+        let tempdir = Builder::new()
+            .prefix("coreos-installer-zipl-bls-")
+            .tempdir()
+            .context("creating temporary directory")?;
+        let firstboot_file = boot.join("ignition.firstboot");
+        let blsdir = if firstboot_file.exists() {
+            let blsdir = tempdir.path().join("loader/entries");
+            create_dir_all(&blsdir).with_context(|| format!("creating {}", blsdir.display()))?;
+            read_dir(boot.join("loader/entries"))
+                .with_context(|| format!("reading {}", boot.display()))?
+                .into_iter()
+                .filter_map(Result::ok)
+                .filter(|p| p.file_type().unwrap().is_file())
+                .for_each(|src| {
+                    copy(src.path(), blsdir.join(src.file_name())).unwrap();
+                });
+            let mut extra = vec!["ignition.firstboot".to_string()];
+            let firstboot_contents = std::fs::read_to_string(&firstboot_file)
+                .with_context(|| format!("reading \"{}\"", firstboot_file.display()))?;
+            if let Some(firstboot_kargs) = extract_firstboot_kargs(&firstboot_contents)? {
+                extra.extend_from_slice(
+                    &firstboot_kargs
+                        .split_whitespace()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<String>>(),
+                );
+            }
+            visit_bls_entry_options(tempdir.path(), |orig_options: &str| {
+                KargsEditor::new()
+                    .append_if_missing(extra.as_slice())
+                    .maybe_apply_to(orig_options)
+            })
+            .with_context(|| format!("appending {:?}", extra))?;
 
-    runcmd!("zipl", "--blsdir", blsdir, "--config", conffile.path())?;
+            blsdir
+        } else {
+            boot.join("loader/entries")
+        };
 
-    Ok(())
+        // create dummy config for zipl
+        let mut conffile = Builder::new()
+            .prefix("coreos-installer-zipl.")
+            .tempfile()
+            .context("creating zipl config")?;
+        let data = format!(
+            "[defaultboot]\ndefaultauto\nprompt=1\ntimeout=5\nsecure=auto\ntarget={}\n",
+            boot.to_str().unwrap()
+        );
+        conffile
+            .write_all(data.as_bytes())
+            .context("writing zipl config")?;
+
+        runcmd!("zipl", "--blsdir", blsdir, "--config", conffile.path())
+    }
 }
 
 /// Returns the first-boot kargs embedded in the contents `s` of a firstboot file.
