4e5c72b234d9424c7f9cd3cdbb5ac2d474ff5a9a
Benjamin Gilbert <bgilbert@redhat.com>
2022-09-09 01:39:37 -0400

    tests: automatically run individual tests on all fixtures they support
    Rather than manually adding new fixtures to every test individually, have
    each test declare its minimum supported fixture, and then automatically
    run the test on any newer fixtures.
    
    Fixes failure to invoke some tests on 2022-02 fixture.

--- tests/images.sh
+++ tests/images.sh
@@ -3,7 +3,15 @@
 set -euo pipefail
 
 export PATH="$(realpath $(dirname $0)/../target/${PROFILE:-debug}):$PATH"
-fixtures="$(realpath $(dirname $0)/../fixtures)"
+fixturesdir="$(realpath $(dirname $0)/../fixtures)"
+
+fixtures=(
+    embed-areas-2020-09.iso.xz
+    embed-areas-2021-01.iso.xz
+    embed-areas-2021-09.iso.xz
+    embed-areas-2021-12.iso.xz
+    embed-areas-2022-02.iso.xz
+)
 
 msg() {
     cat <<EOF
@@ -19,6 +27,23 @@ call() {
     local cmd="$1"
     shift
     "$(dirname $0)/images/${cmd}" "$@"
+}
+
+call_for_fixtures() {
+    local cmd="$1" first_fixture="$2"
+    local fixture run=0
+    for fixture in "${fixtures[@]}"; do
+        if [ "$fixture" = "$first_fixture" ]; then
+            run=1
+        fi
+        if [ $run = 1 ]; then
+            call "$cmd" "${fixturesdir}/iso/${fixture}"
+        fi
+    done
+    if [ $run = 0 ]; then
+        echo "Unknown fixture ${first_fixture}; typo?"
+        exit 1
+    fi
 }
 
 if [ -n "${1:-}" ]; then
@@ -38,17 +63,10 @@ if [ -n "${1:-}" ]; then
 fi
 
 # test historical layouts using fixtures
-call iso-ignition.sh ${fixtures}/iso/embed-areas-2020-09.iso.xz
-call iso-ignition.sh ${fixtures}/iso/embed-areas-2021-01.iso.xz
-call iso-ignition.sh ${fixtures}/iso/embed-areas-2021-09.iso.xz
-call iso-ignition.sh ${fixtures}/iso/embed-areas-2021-12.iso.xz
-call iso-network.sh ${fixtures}/iso/embed-areas-2021-12.iso.xz
-call iso-kargs.sh ${fixtures}/iso/embed-areas-2021-01.iso.xz
-call iso-kargs.sh ${fixtures}/iso/embed-areas-2021-09.iso.xz
-call iso-kargs.sh ${fixtures}/iso/embed-areas-2021-12.iso.xz
-call iso-kargs.sh ${fixtures}/iso/embed-areas-2022-02.iso.xz
-call iso-extract-minimal-iso.sh ${fixtures}/iso/embed-areas-2021-12.iso.xz
-call iso-extract-minimal-iso.sh ${fixtures}/iso/embed-areas-2022-02.iso.xz
+call_for_fixtures iso-ignition.sh embed-areas-2020-09.iso.xz
+call_for_fixtures iso-network.sh embed-areas-2021-12.iso.xz
+call_for_fixtures iso-kargs.sh embed-areas-2021-01.iso.xz
+call_for_fixtures iso-extract-minimal-iso.sh embed-areas-2021-12.iso.xz
 call unsupported.sh
 
 # other image tests
